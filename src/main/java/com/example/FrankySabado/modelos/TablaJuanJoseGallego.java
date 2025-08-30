cb6b69dabc27d4cfdc60c815d5dcf34088d45338
Alexandra <alexacuartas@gmail.com>
2025-08-30 09:31:17 -0500

    feat se crea tablajuangallego

---
+++ src/main/java/com/example/FrankySabado/modelos/TablaJuanJoseGallego.java
@@ -0,0 +1,14 @@
+package com.example.FrankySabado.modelos;
+
+public class TablaJuanJoseGallego {
+
+    //ID
+    private Integer id;
+    //NOMBRE
+    //DIRECCION
+
+    //CONSTR //SET //GET
+
+    //RELACION 1 A UNO 1
+
+}
