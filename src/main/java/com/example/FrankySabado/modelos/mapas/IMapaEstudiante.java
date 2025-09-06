d54c8eaf895a87583b39b2b72a82c91178fc0bd0
Alexandra <alexacuartas@gmail.com>
2025-09-06 11:03:21 -0500

    se crea repositorio y DTO

---
+++ src/main/java/com/example/FrankySabado/modelos/mapas/IMapaEstudiante.java
@@ -0,0 +1,21 @@
+package com.example.FrankySabado.modelos.mapas;
+
+import com.example.FrankySabado.modelos.Estudiante;
+import com.example.FrankySabado.modelos.dtos.EstudianteDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface IMapaEstudiante {
+
+    @Mapping(source = "usuario.nombre", target = "nombre")
+    @Mapping(source = "usuario.rol", target = "rol")
+    EstudianteDTO convertirModeloADto(Estudiante estudiante);
+
+    //Mapeo de Listas
+    List<EstudianteDTO> convertirListaADto(List<Estudiante> lista);
+
+
+}
