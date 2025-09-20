529476b5d25ea811d5312713aa0085866da294cf
eravebo <evelynraveb@gmail.com>
2025-09-20 09:24:27 -0500

    se cera controlador usuario

---
+++ src/main/java/com/example/FrankySabado/controladores/UsuarioControlador.java
@@ -0,0 +1,42 @@
+package com.example.FrankySabado.controladores;
+
+import com.example.FrankySabado.modelos.Usuario;
+import com.example.FrankySabado.servicios.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios") //ACA BAUTIZO EL SERVICIO(API)
+public class UsuarioControlador {
+
+    //Llamar al servicio
+    //Inyectar la dependencia al servicio
+    @Autowired
+    UsuarioServicio servicio;
+
+    //En el controlador activo la recepcion
+    //y el envio de respuestas hacia el cliente
+    //por cada funcion que tenga en mi servicio
+
+    //1.Activando el API para guardar 1 usuario
+    @PostMapping
+    public ResponseEntity<?>activarPeticionGuardar(@RequestBody Usuario datos){
+        try{
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(this.servicio.guardarUsuarioGenerico(datos));
+        }catch (Exception error){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(error.getMessage());
+
+        }
+
+    }
+
+}
